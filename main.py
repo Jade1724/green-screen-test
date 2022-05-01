21c5955ab6165b0b8497d3d8ae455474439aad9d
Jade1724 <haruka.i1724@gmail.com>
2022-05-01 17:44:38 +1200

    Created a mask on green screen video

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 video = cv2.VideoCapture("green-screen-video.mp4")
 image = cv2.imread("japanese-garden.jpg")
@@ -8,7 +9,7 @@ def nothing():
     pass
 
 
-cv2.nameWindow("Trackbars")
+cv2.namedWindow("Trackbars")
 cv2.resizeWindow("Trackbars", 300, 300)
 
 cv2.createTrackbar('L - H', "Trackbars", 0, 179, nothing)
@@ -21,7 +22,7 @@ cv2.createTrackbar('U - V', "Trackbars", 225, 225, nothing)
 while True:
     ret, frame = video.read()
     frame = cv2.resize(frame, (640, 480))
-    frame = cv2.resize(image, (640, 480))
+    image = cv2.resize(image, (640, 480))
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     l_h = cv2.getTrackbarPos('L - H', "Trackbars")
     l_s = cv2.getTrackbarPos('L - S', "Trackbars")
@@ -29,9 +30,12 @@ while True:
     u_h = cv2.getTrackbarPos('U - H', "Trackbars")
     u_s = cv2.getTrackbarPos('U - S', "Trackbars")
     u_v = cv2.getTrackbarPos('U - V', "Trackbars")
-    
-    mask = cv2.inRange(hsv, )
+    l_green = np.array([l_h, l_s, l_v])
+    u_green = np.array([u_h, u_s, u_v])
+
+    mask = cv2.inRange(hsv, l_green, u_green)
     cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", mask)
     k = cv2.waitKey(1)
     if k == ord('q'):
         break
